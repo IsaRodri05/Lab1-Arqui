fc32b8077656e15aec018f5efd42cd826dc50d2b
IsaRodri05 <misabellara0510@hotmail.com>
2025-05-06 21:24:25 -0500

    Feat: Progress to step 8.10 (entities created)

---
+++ DotNetApplication/Lab1App/Models/Entities/Telefono.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1App.Models.Entities;
+
+public partial class Telefono
+{
+    public string Num { get; set; } = null!;
+
+    public string Oper { get; set; } = null!;
+
+    public int Duenio { get; set; }
+
+    public virtual Persona DuenioNavigation { get; set; } = null!;
+}
